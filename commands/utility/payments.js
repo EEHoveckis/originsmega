f7501271d3e9259b366fb12231719f72efa31826
Edgars Eduards Hoveckis <eehoveckis@protonmail.com>
2025-05-12 22:52:29 +0100

    1.0.2 - Updated Payments

--- commands/utility/payments.js
+++ commands/utility/payments.js
@@ -13,24 +13,33 @@ module.exports = {
 		const sourcer = interaction.options.getString("sourcer");
 
 		const userData = await database.collection("payments").findOne({ sourcer: sourcer });
-		let totalPay = 0;
-		let paymentsData = "";
-		for (var i = 0; i < userData.jobs.length; i++) {
-			paymentsData += `${userData.jobs[i].jobDesc} - ${userData.jobs[i].payment}<:ruby:1365502815807602808>\n`;
-			totalPay += userData.jobs[i].payment;
+		if (userData == null) {
+			const errorEmbed = new EmbedBuilder()
+				.setColor("#c21717")
+				.setTitle("ERROR")
+				.setDescription(`Data for **${sourcer}** not found!`);
+
+			await interaction.reply({ embeds: [errorEmbed] });
+		} else {
+			let totalPay = 0;
+			let paymentsData = "";
+			for (var i = 0; i < userData.jobs.length; i++) {
+				paymentsData += `${userData.jobs[i].jobDesc} - ${userData.jobs[i].payment}<:ruby:1365502815807602808>\n`;
+				totalPay += userData.jobs[i].payment;
+			}
+
+			const sourcerUUID = await fetchUUID(sourcer);
+
+			const paymentsEmbed = new EmbedBuilder()
+				.setColor("#7b11bd")
+				.setTitle(`Sourcer: ${sourcer}`)
+				.setDescription(paymentsData)
+				.addFields({ name: "TOTAL EARNINGS:", value: `${totalPay}<:ruby:1365502815807602808>` })
+				.setThumbnail(`https://api.mineatar.io/face/${sourcerUUID}?scale=32`)
+				.setTimestamp()
+				.setFooter({ text: `Requested by ${interaction.member.user.username}`, iconURL: interaction.member.user.avatarURL() });
+
+			await interaction.reply({ embeds: [paymentsEmbed] });
 		}
-
-		const sourcerUUID = await fetchUUID(sourcer);
-
-		const paymentsEmbed = new EmbedBuilder()
-			.setColor("#7b11bd")
-			.setTitle(`Sourcer: ${sourcer}`)
-			.setDescription(paymentsData)
-			.addFields({ name: "TOTAL EARNINGS:", value: `${totalPay}<:ruby:1365502815807602808>` })
-			.setThumbnail(`https://api.mineatar.io/face/${sourcerUUID}?scale=32`)
-			.setTimestamp()
-			.setFooter({ text: `Requested by ${interaction.member.user.username}`, iconURL: interaction.member.user.avatarURL() });
-
-		await interaction.reply({ embeds: [paymentsEmbed] });
 	},
 };
