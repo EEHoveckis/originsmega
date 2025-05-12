bcc5f209c3683fe6966fe191cd781c07f0be6650
Edgars Eduards Hoveckis <eehoveckis@protonmail.com>
2025-05-12 23:17:13 +0100

    1.0.3 - Payment polish

--- helperFunctions/addPayment.js
+++ helperFunctions/addPayment.js
@@ -3,6 +3,10 @@ const { operationsIGN } = require("../config.json");
 module.exports = async function(database, sourcer, job, operationsJob) {
 	await database.collection("payments")
 		.updateOne({ sourcer: sourcer }, { $push: { jobs: job } }, { upsert: true });
-	await database.collection("payments")
-		.updateOne({ sourcer: operationsIGN }, { $push: { jobs: operationsJob } }, { upsert: true });
+	if (operationsJob.payment != 0) {
+		await database.collection("payments")
+			.updateOne({ sourcer: operationsIGN }, { $push: { jobs: operationsJob } }, { upsert: true });
+	} else {
+		return;
+	}
 };
